b08678dd244d99bd051945a45c2181d6b31a5616
Lakshan karunarathna <lakshan.k.muhandhiram@gmail.com>
2026-01-02 23:49:28 +0530

    Add floating shapes to partners page
    Introduced a 'floating-shapes' div with multiple shape elements to the partners.php page for enhanced visual design. Also added missing body and html closing tags.

--- public/pages/partners.php
+++ public/pages/partners.php
@@ -6,4 +6,14 @@
     <title>Our Partners</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     
+
 </head>
+<body>
+<div class="floating-shapes">
+        <div class="shape"></div>
+        <div class="shape"></div>
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+</body>
+</html>
