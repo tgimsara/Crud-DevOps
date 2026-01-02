68fb2780c9859adbf705c7f399dda57ebb29c33b
Pahan Perera <pahanperera21a@gmail.com>
2026-01-03 01:39:35 +0530

    Add plant trees page and goal CSS, update carbon reduction title
    Added a new CSS file (goal.css) for goal-related page styling and created a new planttrees.php page. Updated the title in carbonreduction.php to 'Reducing Carbon Dioxide Emissions' for accuracy.

---
+++ public/pages/planttrees.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="../css/goal.css" rel = "stylesheet">
+
+    <title></title>
+
+
+    
