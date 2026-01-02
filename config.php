cea598b7ece4569363fa44fe72dc21dff31e0f7e
Lakshan karunarathna <lakshan.k.muhandhiram@gmail.com>
2026-01-02 22:48:14 +0530

    Add database connection to config.php
    Introduced MySQL database connection parameters and connection logic in config.php. The script now establishes a connection to the 'crud_devops_database' and terminates with an error message if the connection fails.

--- config.php
+++ config.php
@@ -1,1 +1,15 @@
  
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "crud_devops_database";
+
+
+  $conn = mysqli_connect($host, $user, $password, $database);
+
+
+  if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+   }
+ 
