f80fa9f8dfb97b5b8b683165935bc427f9d3705d
Lakshan karunarathna <lakshan.k.muhandhiram@gmail.com>
2026-01-03 01:13:17 +0530

    Update Management_Page.php

--- public/pages/Management_Page.php
+++ public/pages/Management_Page.php
@@ -388,5 +388,14 @@
     </div>
 
 
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
+    <script>
+
+
+
+        
+    </script>   
+
     </body>
 </html>
