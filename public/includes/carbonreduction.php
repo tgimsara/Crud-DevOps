7f2868e48984b482496773b6cccc02a237426b85
Pahan Perera <pahanperera21a@gmail.com>
2026-01-02 23:20:06 +0530

    Add carbonreduction.php and update DB schema
    Added a new carbonreduction.php HTML template in public/includes. Updated db_schema_only.sql to include a users table.

---
+++ public/includes/carbonreduction.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Achieve Gender Equality</title>
+
+</head>
+
+
+
+<body>
+
+
+
+
+
+
+</body>
+
+</html>
