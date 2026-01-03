6480935b49f46cdddc3b6a0baaa8ce0667547b53
Tharusha Gimsara <eatharusha@gmail.com>
2026-01-03 08:24:05 +0530

    up

--- public/database/crud_devops_database (1).sql
+++ public/database/crud_devops_database (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 03, 2026 at 02:09 AM
+-- Generation Time: Jan 03, 2026 at 03:26 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
